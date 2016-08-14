172736c0fa4c5c1a149e8eb82917ce41f1af8baa
Moti Zilberman <motiz88@gmail.com>
2016-08-14 22:32:43 +0300

    chore: update deploy script again

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,8 @@ git config user.email "$GH_NAME"
 git config user.name "$GH_EMAIL"
 git commit -m "feat: regenerate with $PACKAGE_NAME@$PACKAGE_VERSION" "--author=$GH_NAME <$GH_EMAIL>"
 
-which hub || gem install hub
+mkdir -p hub
+curl https://github.com/github/hub/releases/download/v2.2.4/hub-linux-amd64-2.2.4.tgz -sL | tar zxf - -C hub --strip-components 1
 
 mkdir -p ~/.config
 if [[ ! -e ~/.config/hub ]]; then cat > ~/.config/hub <<CONFIG
@@ -29,9 +30,9 @@ if [[ ! -e ~/.config/hub ]]; then cat > ~/.config/hub <<CONFIG
 github.com:
 - protocol: https
   user: $GH_USER
-  oauth_token: $GH_TOKEN
+  oauth_token: $GH_BOT_TOKEN
 CONFIG
 fi
 
-hub push origin  $TARGET_BRANCH
-hub pull-request -m "Use $PACKAGE_NAME@$PACKAGE_VERSION"
+hub/bin/hub push origin  $TARGET_BRANCH
+hub/bin/hub pull-request -m "Use $PACKAGE_NAME@$PACKAGE_VERSION"
